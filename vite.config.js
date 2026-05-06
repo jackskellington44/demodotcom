8e5d9946cddf216fa41f156533300e205632cce4
jackskellington44 <jackskellington4@duck.com>
2026-05-06 18:40:52 +0200

    ahhhh

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/demo5/",
+  base: "/demo1/",
   build: {
     rollupOptions: {
       input: {
